577ae58393f590abd87896935ba8da65725ba4f0
mobb <cooldownlengque@163.com>
2020-11-02 22:37:48 +0800

    {_01_NetworkCharacteristics.py : add : implementation of solvation; }

--- Homework01/_01_NetworkCharacteristics.py
+++ Homework01/_01_NetworkCharacteristics.py
@@ -24,7 +24,8 @@ def _01_solve():
             i += 1
     
     nx.draw(g, with_labels=True)
-    plt.savefig('_01_Erd ̋os-R ́enyiRandomGraph.png')
+    # plt.savefig('_01_Erd ̋os-R ́enyiRandomGraph.png')
+    return g
 
 
 def _02_solve():
@@ -57,7 +58,9 @@ def _02_solve():
             
 
     nx.draw(g, with_labels=True)
-    plt.savefig('_02_SmallWorld.png')
+    # plt.savefig('_02_SmallWorld.png')
+
+    return g
 
 
 def _03_solve():
@@ -66,15 +69,67 @@ def _03_solve():
 
     # 2. 采样
     g = nx.DiGraph(random.sample(g.edges(), 80))
-    print(g.edges())
+    # print(g.edges())
+
+    return g
+
+
+def _11_solve():
+    g1 = _01_solve()
+    g2 = _02_solve()
+    g3 = _03_solve()
+
+    g = nx.Graph()
+    g.add_edges_from(g1.edges())
+    g.add_edges_from(g2.edges())
+
+    # print(g.edges())
+
+
+def _12_solve():
+    g1 = _01_solve()
+    g2 = _02_solve()
+
+    g = nx.DiGraph()
+    g.add_edges_from(g1.edges())
+    g.add_edges_from(g2.edges())
+
+    
+    coe = dict()
+    for node in g.nodes():
+        k = 0
+        neighbors = []
+        for src, dst in g.edges():
+            if src == node:
+                k += 1
+                neighbors += [dst]
+        
+        if k >= 2:
+            c = 0
+            for a in g.nodes():
+                for b in g.nodes():
+                    if (a, b) in g.edges() or (b, a) in g.edges():
+                        c += 1
+            coe[node] = 2 * c / (k * (k - 1))
+        else:
+            coe[node] = 0
+
+    C = sum(coe.values()) / len(coe)
+    return C, coe
 
 
 if __name__ == "__main__":
-    # 1. Erd ̋os-R ́enyi Random graph
-    # _01_solve()
+    # 0.1. Erd ̋os-R ́enyi Random graph
+    _01_solve()
 
-    # 2. Small-World Random Network
-    # _02_solve()
+    # 0.2. Small-World Random Network
+    _02_solve()
 
-    # 3. Real-World Collaboration Network
+    # 0.3. Real-World Collaboration Network
     _03_solve()
+
+    # 1.1. Degree Distribution
+    _11_solve()
+
+    # 1.2. Clustering
+    _12_solve()
