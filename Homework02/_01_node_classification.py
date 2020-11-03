736179a9721767a565fb7b28d1c4896f368d9edf
mobb <cooldownlengque@163.com>
2020-11-03 17:15:47 +0800

    {_01_node_classification.py : add : worked out the first problem ; }

--- Homework02/_01_node_classification.py
+++ Homework02/_01_node_classification.py
@@ -1,8 +1,46 @@
 #! /usr/bin/env python3
 # -*- coding:utf-8 -*-
-import networkx
-import matplotlib
+import networkx as nx
+import matplotlib.pyplot as plt
+
+import random
+
+
+def _11_node_classification():
+    loops = 2
+
+    nodes = list(range(1, 11))
+    edges = [
+        (1, 2), (1, 3), (2, 3), (2, 4), (3, 6), (5, 6), (4, 7),
+        (4, 8), (5, 8), (5, 9), (7, 8), (6, 9), (6, 10), (9, 10)
+    ]
+
+    # build the graph and initialize it with Pr = 0.5
+    g = nx.Graph()
+    g.add_nodes_from(nodes, Pr=0.5)
+    g.add_edges_from(edges)
+
+    # initialize postive nodes
+    g.nodes[3]["Pr"] = 1
+    g.nodes[5]["Pr"] = 1
+    # initialize negative nodes
+    g.nodes[8]["Pr"] = 0
+    g.nodes[10]["Pr"] = 0
+
+    nx.draw(g, with_labels=True)
+    plt.savefig('_01_node_classification.png')
+
+    # get the possibility of every node to be positive
+    [nodes.remove(x) for x in [3, 5, 8, 10]]
+    for i in range(loops):
+        for node in nodes:
+            g.nodes[node]["Pr"] = sum([g.nodes[node]["Pr"] for node in g[node]]) / len(g[node])
+
+    return  g
+
 
 
 if __name__ == "__main__":
-    print("Hi python")
+    # 1.1. relational classification
+    ans = _11_node_classification()
+    print(ans.nodes.data())
